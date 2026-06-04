1b384fe656e60e946987a6a3cfee63f3e4ee8cbe
stron <yaneliyahu83@gmail.com>
2026-06-04 21:06:39 +0300

    chore: change routing to be with the const

--- src/pages/error/error.tsx
+++ src/pages/error/error.tsx
@@ -1,6 +1,7 @@
 import styles from "./Error.module.css";
 import { useRouteError, isRouteErrorResponse, Link } from "react-router";
 import { getErrorMessage } from "../not-found/not-found.utils";
+import { routes } from "~/config/routes/routes";
 
 export function Error() {
   const error: unknown = useRouteError();
@@ -12,7 +13,7 @@ export function Error() {
           {error.status} — {error.statusText}
         </h1>
         <p>{error.data?.message || "An unexpected error occurred."}</p>
-        <Link to="/">Go home</Link>
+        <Link to={routes.home}>Go home</Link>
       </div>
     );
   }
@@ -23,7 +24,7 @@ export function Error() {
     <div className={styles.errorContainer}>
       <h1>Something went wrong</h1>
       <p>{errorMessage}</p>
-      <Link to="/">Go home</Link>
+      <Link to={routes.home}>Go home</Link>
     </div>
   );
 }
--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,11 +1,12 @@
 import styles from "./not-found.module.css";
 import { Link } from "react-router";
+import { routes } from "~/config/routes/routes";
 
 export function NotFound() {
   return (
     <div className={styles.notFound}>
       <h1>404 — Page Not Found</h1>
-      <Link to="/">Go home</Link>
+      <Link to={routes.home}>Go home</Link>
     </div>
   );
 }
